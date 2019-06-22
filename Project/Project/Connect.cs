b051c6e69f48705aa5387a556252bf92098bd2ba
Koxpit <30499991+Koxpit@users.noreply.github.com>
2019-06-22 21:12:53 +0300

    Project_1
    Project_1_Add

---
+++ Project/Project/Connect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public class Connect
+    {
+        public static SqlConnection CreateConnection()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source = USER-ПК\SQLEXPRESS; Integrated Security = SSPI; Initial Catalog = Алиев");
+            //connection.ConnectionString = @"Data Source = USER-ПК\SQLEXPRESS; Integrated Security = SSPI; Initial Catalog = Алиев";
+            try
+            {
+                connection.Open();
+             //   MessageBox.Show("Успешное подключение");
+            } catch
+            {
+                MessageBox.Show("Не удалось установить соединение.");
+                Environment.Exit(0);
+            }
+            return connection;
+        }
+     //   Data Source = USER - ПК\SQLEXPRESS;Integrated Security = SSPI; Initial Catalog = Алиев
+    }
+}
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void OutputText()
+        {
+            string sql = $"select * from Ткань where Артикул='16314397'";
+            SqlConnection con = Connect.CreateConnection();
+            SqlCommand command = new SqlCommand(sql, con);
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                textBox2.Text = reader.GetValue(1).ToString();
+            } else MessageBox.Show("Нет данных");
+            con.Close();
+        }
+        public void Output()
+        {
+           // string sql = "select * from Ткань";
+            //SqlConnection con = Connect.CreateConnection();
+            SqlCommand command = new SqlCommand("select * from Ткань", Connect.CreateConnection());
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable dt = new DataTable();
+            if (reader.HasRows)
+            {
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+            }
+            else MessageBox.Show("Нет данных");
+            Connect.CreateConnection().Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Output();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OutputText();
+        }
+    }
+}
